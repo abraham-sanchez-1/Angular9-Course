6ceef0c4b9bcd4573a1c3a79b6f9c30f8d020fa1
Abe Sanchez <sanchez95san@gmail.com>
2020-05-27 15:42:21 -0500

    Ability to add items on recipe to shopping list has now been enabled

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,27 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('Sweet Shrimp','Tasty shrimp on a bed of zesty rice','https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg', [new Ingredient('Shrimp', 10), new Ingredient('Rice', 50)] ),
-    new Recipe('Steak Dream','Tasy steak in beautiful sauce','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg' , [new Ingredient('Steak', 1), new Ingredient('Saunce', 10)] )
+    new Recipe('Steak Dream','Tasy steak in beautiful sauce','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg' , [new Ingredient('Steak', 1), new Ingredient('Sauce', 10)] )
   ];
 
+  constructor(private slService: ShoppingListService) {
+
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+
   }
 }
