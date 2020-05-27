420deaa3ddc730f1392ba8d2f080f24bd5162be6
Abe Sanchez <sanchez95san@gmail.com>
2020-05-27 15:24:08 -0500

    Shopping list functionality added, ingredients now located in service and now also display when item is selected, will work on sending items to shopping list next

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
+  constructor(private slService: ShoppingListService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
   }
-
-onAddItem() {
-  const ingName = this.nameInputRef.nativeElement.value;
-  const ingAmount = this.amountInputRef.nativeElement.value;
-  const newIngredient = new Ingredient(ingName, ingAmount);
-  this.ingredientAdded.emit(newIngredient);
-}
-
 }
